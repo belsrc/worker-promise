0f065ae287ed8f1568255d37c0396b81573394c2
Bryan Kizer <bryan@bryanckizer.com>
2020-08-19 12:18:53 -0400

    ci: update actions configs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,7 +3,6 @@ module.exports = {
     'prettier --config .prettierrc.js --write',
     'eslint -c .eslintrc.js --fix --ignore-path .eslintignore',
     'cross-env NODE_ENV=test jest --findRelatedTests --passWithNoTests',
-    'git add',
   ],
-  '*.{md,html,json}': ['prettier --config .prettierrc.js --write', 'git add'],
+  '*.{md,html,json}': ['prettier --config .prettierrc.js --write'],
 };
